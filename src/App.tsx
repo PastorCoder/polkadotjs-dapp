d2c79f6cc865b3fbcca9f4e993383d6b62d6de9d
pastorcoder <zuma4good@gmail.com>
2023-04-11 02:11:50 +0100

    lesson 3 - How to interact with the wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,61 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { WsProvider, ApiPromise } from '@polkadot/api';
+
+import { web3Enable, web3Accounts } from "@polkadot/extension-dapp";
+import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
+
+
+const NAME = "GmOrDie";
 
 const App = () => {
   const [api, setApi] = useState<ApiPromise>();
+  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+  const [selectedAccount, setSelectedAccount] = useState<InjectedAccountWithMeta>();
 
-  const setup = async () => { 
+  const setup = async () => {
     const wsProvider = new WsProvider("wss://ws.gm.bldnodes.org/");
     const api = await ApiPromise.create({ provider: wsProvider });
     setApi(api);
-    console.log(api);
-  }
+    // console.log(api);
+  };
+
+
+  const handleConnection = async () => {
+    const extensions = await web3Enable(NAME);
+
+    if (!extensions) {
+      throw Error("No_EXTENSION_FOUND")
+    };
+
+    const allAccounts = await web3Accounts();
+
+    console.log("All Connected Accounts is : ", extensions)
+
+    
+
+    if (allAccounts.length === 0) {
+      setSelectedAccount(allAccounts[0]);
+    }
+
+    setAccounts(allAccounts);
+  };
+
+
+  const handleAccountSelection = async (e: ChangeEvent<HTMLSelectElement>) => { 
+    const selectedAddress = e.target.value;
+
+    const account = accounts.find((account) => account.address === selectedAddress);
+
+    if (!account) {
+      throw Error("NO_ACCOUNT_FOUND");
+    }
+
+    setSelectedAccount(account);
+  };
+
+
+
+
 
   useEffect(() => {
     setup();
@@ -29,7 +75,20 @@ const App = () => {
 
   return (
     <div>
-      Dapp
+      {accounts.length === 0 ? <button onClick={handleConnection}>Connect</button> : null}
+
+      {accounts.length > 0 && !selectedAccount ? (
+        <>
+          <select onChange={handleAccountSelection}>
+            <option value="" disabled selected hidden>
+              Choose your account
+            </option>
+          {accounts.map((account) => <option value={account.address}>{account.address}</option>)}
+        </select>
+        </>) : null}
+
+      {/**{accounts.length > 0 && selectedAccount ? selectedAccount.address : null} */}
+      {selectedAccount ? selectedAccount.address : null}
     </div>
   )
 }
